22ae6147c184c6f5c7955109766e00accdc23d74
Thomas shikalepo deon <thomasshikalepo@gmail.com>
2024-12-06 02:49:11 +0200

    started with the about me section

--- src/app/(Sub pages)/about/page.js
+++ src/app/(Sub pages)/about/page.js
@@ -1,7 +1,5 @@
 import Image from "next/image";
-import bg from "../../../../public/background/background.webp";
-import { projectsData } from "../../data";
-import ProjectList from "@/components/projects";
+import bg from "../../../../public/background/about-background.png";
 import Staff from "@/components/models/Staff";
 import RenderModel from "@/components/RenderModel";
 
@@ -14,7 +12,7 @@ export default function Home() {
         className="-z-50 fixed top-0 left-0 w-full h-full object-cover object-center opacity-25"
       />
 
-      <ProjectList projects={projectsData} />
+      
 
       <div className="flex items-center justify-center fixed top-20 -left-24 h-screen">
       <RenderModel >
