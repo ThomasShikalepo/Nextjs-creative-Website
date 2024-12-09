94bce13ab5fdc856cd23862f5408524885e8b182
Thomas shikalepo deon <thomasshikalepo@gmail.com>
2024-12-09 12:18:26 +0200

    😭😭😭

--- src/app/(Sub pages)/contact/page.js
+++ src/app/(Sub pages)/contact/page.js
@@ -2,14 +2,13 @@ import Image from "next/image";
 import bg from "../../../../public/background/contact-background.png";
 import Form from "@/components/contacts/Form";
 
-
 export default function Contact() {
   return (
     <main>
       {/* Background Image */}
       <Image
         src={bg}
-        sizes="100vw "
+        sizes="100vw"
         alt="background-image"
         className="-z-50 fixed top-0 left-0 w-full h-full object-cover object-center opacity-25"
         layout="fill" // Ensures it covers the entire viewport size
@@ -24,12 +23,11 @@ export default function Contact() {
           <p className="text-center font-light text-lg xs:text-xl">
             Step into the circle of magic and share your message. <br />
             Whether you are seeking collaboration, answers to mysteries,<br /> or simply wish to connect,
-            your words are treasures in this realm.Use the form below to send your missive, and await a magical reply.
+            your words are treasures in this realm&apos;s Use the form below to send your missive, and await a magical reply.
           </p>
-        </div> 
+        </div>
         <Form />
       </article>
-
     </main>
   );
 }
